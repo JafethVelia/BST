a4f5591f3e30a9e513432ea1ec3b0dc084706cd2
Jafeth <PC USER@DESKTOP-4L510NH>
2025-04-05 21:18:36 -0600

    Agregar archivos de proyecto.

---
+++ BinarySearchTreeLib/Class1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTreeLib
+{
+    public class Class1
+    {
+        public class TreeNode
+        {
+            public int Key;
+            public TreeNode Left, Right;
+
+            public TreeNode(int key)
+            {
+                Key = key;
+                Left = Right = null;
+            }
+        }
+
+        public class BinarySearchTree
+        {
+            public TreeNode Root;
+
+            public BinarySearchTree()
+            {
+                Root = null;
+            }
+
+            // Inserción
+            public void Insert(int key)
+            {
+                Root = InsertRecursive(Root, key);
+            }
+
+            private TreeNode InsertRecursive(TreeNode node, int key)
+            {
+                if (node == null) return new TreeNode(key);
+                if (key < node.Key) node.Left = InsertRecursive(node.Left, key);
+                else if (key > node.Key) node.Right = InsertRecursive(node.Right, key);
+                return node;
+            }
+
+            // Búsqueda
+            public bool Search(int key)
+            {
+                return SearchRecursive(Root, key);
+            }
+
+            private bool SearchRecursive(TreeNode node, int key)
+            {
+                if (node == null) return false;
+                if (node.Key == key) return true;
+                return key < node.Key
+                    ? SearchRecursive(node.Left, key)
+                    : SearchRecursive(node.Right, key);
+            }
+
+            // Eliminación
+            public void Delete(int key)
+            {
+                Root = DeleteRecursive(Root, key);
+            }
+
+            private TreeNode DeleteRecursive(TreeNode node, int key)
+            {
+                if (node == null) return null;
+
+                if (key < node.Key) node.Left = DeleteRecursive(node.Left, key);
+                else if (key > node.Key) node.Right = DeleteRecursive(node.Right, key);
+                else
+                {
+                    if (node.Left == null) return node.Right;
+                    if (node.Right == null) return node.Left;
+
+                    node.Key = MinValue(node.Right);
+                    node.Right = DeleteRecursive(node.Right, node.Key);
+                }
+                return node;
+            }
+
+            private int MinValue(TreeNode node)
+            {
+                int minv = node.Key;
+                while (node.Left != null)
+                {
+                    node = node.Left;
+                    minv = node.Key;
+                }
+                return minv;
+            }
+
+            // Recorrido In-Order
+            public List<int> InOrder()
+            {
+                var result = new List<int>();
+                InOrderRecursive(Root, result);
+                return result;
+            }
+
+            private void InOrderRecursive(TreeNode node, List<int> result)
+            {
+                if (node != null)
+                {
+                    InOrderRecursive(node.Left, result);
+                    result.Add(node.Key);
+                    InOrderRecursive(node.Right, result);
+                }
+            }
+        }
+
+    }
+}
---
+++ BinarySearchTreeTests/Test1.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using BinarySearchTreeLib; 
+using static BinarySearchTreeLib.Class1;
+
+namespace BinarySearchTreeTests
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void InsertAndSearchTest()
+        {
+            var bst = new BinarySearchTree();
+            bst.Insert(8);
+            bst.Insert(3);
+            bst.Insert(10);
+            bst.Insert(1);
+
+            Assert.IsTrue(bst.Search(8));
+            Assert.IsTrue(bst.Search(1));
+            Assert.IsFalse(bst.Search(99));
+        }
+
+        [TestMethod]
+        public void DeleteLeafNodeTest()
+        {
+            var bst = new BinarySearchTree();
+            bst.Insert(5);
+            bst.Insert(3);
+            bst.Insert(7);
+            bst.Delete(3);
+
+            Assert.IsFalse(bst.Search(3));
+        }
+
+        [TestMethod]
+        public void DeleteNodeWithOneChildTest()
+        {
+            var bst = new BinarySearchTree();
+            bst.Insert(5);
+            bst.Insert(3);
+            bst.Insert(2);
+            bst.Delete(3);
+
+            Assert.IsFalse(bst.Search(3));
+            Assert.IsTrue(bst.Search(2));
+        }
+
+        [TestMethod]
+        public void DeleteNodeWithTwoChildrenTest()
+        {
+            var bst = new BinarySearchTree();
+            bst.Insert(5);
+            bst.Insert(3);
+            bst.Insert(7);
+            bst.Insert(6);
+            bst.Insert(8);
+
+            bst.Delete(7);
+
+            Assert.IsFalse(bst.Search(7));
+            Assert.IsTrue(bst.Search(6));
+            Assert.IsTrue(bst.Search(8));
+        }
+
+        [TestMethod]
+        public void InOrderTraversalTest()
+        {
+            var bst = new BinarySearchTree();
+            int[] values = { 8, 3, 10, 1, 6, 4, 7, 14, 13 };
+            foreach (int v in values) bst.Insert(v);
+
+            var expected = new List<int> { 1, 3, 4, 6, 7, 8, 10, 13, 14 };
+            CollectionAssert.AreEqual(expected, bst.InOrder());
+        }
+    }
+}
